d1d4be0a1b4779c931e78265a3f764e77bd47a55
Julien Peeters <jj.peeters@gmail.com>
2009-03-27 22:52:48 +0100

    Cleanning of repository --Julien

--- I2C.h
+++
@@ -1,6 +0,0 @@
-#include <clib.h>
-#define SDA_PIN 1	//set PIO1 as SDA
-#define SCL_PIN 0	//set PIO0 as SCL
-void init_i2c(void);
-int I2C_write(unsigned char slave, char char_out);
-int I2C_read(unsigned char slave, char far *buffer, int length);
