51866bdf1830b61423116ea11dd8eef030aad8ca
Julien Peeters <jj.peeters@gmail.com>
2009-04-02 10:42:36 +0200

    nouvelle version

---
+++ i2c/i2c.c
@@ -0,0 +1,24 @@
+/*
+// Sample C code - customize as your Beck IPC application
+*/
+
+#include "I2C.h"
+
+void i2c_init()
+{
+	I2C_init();
+}
+
+int i2c_write(unsigned char slave, const char far *buffer, int length)
+{
+	return I2C_transmit_block(slave, buffer, length);
+}
+
+int i2c_read(unsigned char slave, char far *buffer, int length)
+{
+	return I2C_receive_block(slave, buffer, length);
+}
+void i2c_destroy()
+{
+    I2C_release();
+}
