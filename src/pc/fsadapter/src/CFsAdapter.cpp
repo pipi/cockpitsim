97d3f1263d8e0a239443643e090a8fc057b4be81
Julien Peeters <jj.peeters@gmail.com>
2009-04-22 08:40:28 +0200

    Adding debug messages into FSAdapter.cpp and main.cpp.
    --Julien.

--- src/pc/fsadapter/src/CFsAdapter.cpp
+++ src/pc/fsadapter/src/CFsAdapter.cpp
@@ -4,18 +4,44 @@
 
 #include "CFsAdapter.h"
 
+#ifdef _DEBUG
+#include <iostream>
+#endif
+
 void CFsAdapter::lookupForCanMessages() {
 	can_event_msg_t msg;
+
+#ifdef _DEBUG
+	std::cout << "Lookup for CAN messages..." << std::endl;
+	unsigned int msgCnt = 0;
+#endif
+
 	while(can_recv(1, &msg) == 0) { // empty the input queue.
+		
+#ifdef _DEBUG
+		std::cout << "CAN message received with id " 
+			<< msg.id << "." << std::endl;
+		msgCnt++;
+#endif
+
 		for(std::list<CAbstractOffsetFamily*>::iterator it =
 				m_lstFamilies.begin(); it != m_lstFamilies.end(); it++) {
 			if((*it)->matchesCanId(msg.id)) {
+
+#ifdef _DEBUG
+				std::cout << "Family found." << std::endl;
+#endif
+
 				(*it)->setValueFromCanMessage(msg);
 				// Because Id <-> Family link is unique.
 				break;
 			}
 		}
 	}
+#ifdef _DEBUG
+	std::cout << "No more messages (" << msgCnt << " messages treated)."
+		<< std::endl;
+#endif
 }
 
 void CFsAdapter::lookupForFsuipcChanges() {
@@ -35,13 +61,20 @@ CFsAdapter::CFsAdapter(CCanNodeConfig& oNodeConfig,
 	if(!m_oConnector.isOpened() && !m_oConnector.open()) {
 		throw std::exception("Error while opening FSUIPC connection.");
 	}
+#ifdef _DEBUG
+	std::cout << "FSUIPC connection opened." << std::endl;
+#endif
 	if(can_init(oNodeConfig.m_wAcceptanceMask, 
 			oNodeConfig.m_wAcceptanceCode, oNodeConfig.m_wBaudrate) != 0) {
 		throw std::exception("Error while initializing CAN.");
 	}
+#ifdef _DEBUG
+	std::cout << "CAN connection opened." << std::endl;
+#endif
 }
 
 CFsAdapter::~CFsAdapter() {
+	can_destroy();
 	m_oConnector.close();
 }
 
--- src/pc/fsadapter/src/main.cpp
+++ src/pc/fsadapter/src/main.cpp
@@ -14,8 +14,10 @@ int main() {
 		CCanNodeConfig oConfig = { 0xFFFF, 0xFFFF, CAN_BAUDRATE_1M };
 		CFsAdapter oAdapter(oConfig, oConnector);
 	} catch(std::exception& ex) {
+#ifdef _DEBUG
 		std::cerr << ">>> Exception raised <<<" << std::endl << ex.what()
 			<< std::endl;
+#endif
 	}
 
 	return 0;
