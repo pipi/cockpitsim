4ce0140dca0f7c9e4aba03fd88bb63163a4f43b6
Julien Peeters <jj.peeters@gmail.com>
2009-04-22 16:33:47 +0200

    no care

--- lib/i2c/i2c.c
+++ lib/i2c/i2c.c
@@ -16,6 +16,7 @@ int i2c_write(unsigned char slave, const char far *buffer, int length)
 	return I2C_transmit_block(slave, buffer, length);
 }
 
+
 int i2c_read(unsigned char slave, char far *buffer, int length)
 {
 	return I2C_receive_block(slave, buffer, length);
